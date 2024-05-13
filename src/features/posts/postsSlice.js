47a46aadc160f97b29fd39ba343399288d715037
Tomás Arrativel <arrativeltomas@gmail.com>
2024-05-13 17:18:56 -0300

    Making the PostsSlice

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,44 @@
+import {
+  createAsyncThunk,
+  createSelector,
+  createSlice,
+} from '@reduxjs/toolkit';
+
+export const fetchPopularPosts = createAsyncThunk(
+  'posts/fetchPopularPosts',
+  async () => {
+    const response = await fetch('https://www.reddit.com/r/popular.json');
+    const data = await response.json();
+    return data;
+  },
+);
+
+export const PostsSlice = createSlice({
+  name: 'posts',
+  initialState: {
+    posts: [],
+    loading: false,
+    error: false,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPopularPosts.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(fetchPopularPosts.rejected, (state) => {
+        state.loading = false;
+        state.error = true;
+      })
+      .addCase(fetchPopularPosts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = false;
+        state.posts.push(action.payload);
+      });
+  },
+});
+
+export const selectPosts = (state) => state.posts;
+
+export default PostsSlice.reducer;
