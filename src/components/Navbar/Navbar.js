0891b8b3ab5823cdc3eb7741c8b40635035104a7
Tomás Arrativel <arrativeltomas@gmail.com>
2024-05-13 16:07:19 -0300

    Making the menu for mobile

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
-import { IoMdMenu } from 'react-icons/io';
+import { IoMdMenu, IoIosClose } from 'react-icons/io';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOnClick = () => setIsOpen(!isOpen);
+
   return (
     <nav>
-      <div className='nav__logo'>
-        <img src='redditgod-logo.png' alt='Logo' />
-        <h1>
-          Reddit<span>God</span>
-        </h1>
-      </div>
-      <div className='nav__input'>
-        <input type='text' placeholder='Search...' />
-        <button className=''>
-          <FaSearch color='#ecf4ff' />
+      <div className='nav__container'>
+        <div className='nav__logo'>
+          <img src='redditgod-logo.png' alt='Logo' />
+          <h1>
+            Reddit<span>God</span>
+          </h1>
+        </div>
+        <div className='nav__input'>
+          <input type='text' placeholder='Search...' />
+          <button className=''>
+            <FaSearch color='#ecf4ff' />
+          </button>
+        </div>
+        <button onClick={handleOnClick} className='nav__menu'>
+          {!isOpen ? (
+            <IoMdMenu color='#ecf4ff' size={23} />
+          ) : (
+            <IoIosClose color='#ecf4ff' size={23} />
+          )}
         </button>
       </div>
-      <button className='nav__menu'>
-        <IoMdMenu color='#ecf4ff' size={23} />
-      </button>
+
+      <div className={`nav__menu-options ${isOpen ? 'open' : ''}`}></div>
     </nav>
   );
 };
