e6cef11e37defe0ee3c119f275cef9b1178a4967
Tomás Arrativel <arrativeltomas@gmail.com>
2024-05-14 16:23:57 -0300

    Adding comments to any post

--- src/features/posts/Posts.js
+++ src/features/posts/Posts.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchPopularPosts, selectPosts } from './postsSlice';
 
+import { FaRegComment } from 'react-icons/fa';
+
 const Posts = () => {
   const dispatch = useDispatch();
   const postsData = useSelector(selectPosts);
@@ -10,21 +12,23 @@ const Posts = () => {
     dispatch(fetchPopularPosts());
   }, [dispatch]);
 
-  console.log(postsData.posts[0].data.children);
+  console.log(postsData);
+
   return (
     <div className='posts'>
       {postsData.posts.length !== 0 ? (
         postsData.posts[0].data.children.map((post) => (
           <div className='posts__post' key={post.data.id}>
-            {post.data.thumbnail.length > 13 ? (
-              <img
-                src={post.data.thumbnail}
-                alt={`post ${post.data.id} thumbnail`}
-              />
+            <h2>{post.data.title}</h2>
+            {post.data.url.length > 13 ? (
+              <img src={post.data.url} alt={`post ${post.data.id} thumbnail`} />
             ) : (
               ''
             )}
-            <h2>{post.data.title}</h2>
+            <div className='posts__post-comments'>
+              <FaRegComment color='#283f54' size={25} />
+              <p>{post.data.num_comments}</p>
+            </div>
           </div>
         ))
       ) : (
