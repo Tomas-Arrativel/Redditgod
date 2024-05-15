f7d304e14714893f82836b16ba8f6d7bbf5130db
Tomás Arrativel <arrativeltomas@gmail.com>
2024-05-15 16:57:26 -0300

    Changes to get easier styles

--- src/features/posts/Posts.js
+++ src/features/posts/Posts.js
@@ -12,6 +12,8 @@ import {
 import { FaRegComment } from 'react-icons/fa';
 import { LuArrowBigDown, LuArrowBigUp } from 'react-icons/lu';
 
+import './Posts.css';
+
 const Posts = () => {
   const dispatch = useDispatch();
   const postsData = useSelector(selectPosts);
@@ -38,43 +40,46 @@ const Posts = () => {
             <h2>{post.data.title}</h2>
 
             {/*This are the Votation of the people buttons*/}
-            <div className='posts__votes'>
-              <LuArrowBigUp
-                onClick={() => handleUps(post.data.id)}
-                color='#2876d0'
-                size={25}
-                fill={
-                  ups !== undefined && ups.some((id) => id === post.data.id)
-                    ? '#2876d0'
-                    : 'transparent'
-                }
-              />
-
-              <LuArrowBigDown
-                onClick={() => handleDowns(post.data.id)}
-                color='#dd3155'
-                size={25}
-                fill={
-                  downs !== undefined && downs.some((id) => id === post.data.id)
-                    ? '#dd3155'
-                    : 'transparent'
-                }
-              />
-            </div>
-
-            {/*This is the data of the posts (img and comments)*/}
-            <div className='posts__post-info'>
-              {post.data.url.length > 13 ? (
-                <img
-                  src={post.data.url}
-                  alt={`post ${post.data.id} thumbnail`}
+            <div className='posts__container'>
+              <div className='posts__votes'>
+                <LuArrowBigUp
+                  onClick={() => handleUps(post.data.id)}
+                  color='#2876d0'
+                  size={40}
+                  fill={
+                    ups !== undefined && ups.some((id) => id === post.data.id)
+                      ? '#2876d0'
+                      : 'transparent'
+                  }
                 />
-              ) : (
-                ''
-              )}
-              <div className='posts__post-info__comments'>
-                <FaRegComment color='#283f54' size={25} />
-                <p>{post.data.num_comments}</p>
+
+                <LuArrowBigDown
+                  onClick={() => handleDowns(post.data.id)}
+                  color='#dd3155'
+                  size={40}
+                  fill={
+                    downs !== undefined &&
+                    downs.some((id) => id === post.data.id)
+                      ? '#dd3155'
+                      : 'transparent'
+                  }
+                />
+              </div>
+
+              {/*This is the data of the posts (img and comments)*/}
+              <div className='posts__post-info'>
+                {post.data.url.length > 13 ? (
+                  <img
+                    src={post.data.url}
+                    alt={`post ${post.data.id} thumbnail`}
+                  />
+                ) : (
+                  ''
+                )}
+                <div className='posts__post-info__comments'>
+                  <FaRegComment color='#283f54' size={25} />
+                  <p>{post.data.num_comments}</p>
+                </div>
               </div>
             </div>
           </div>
