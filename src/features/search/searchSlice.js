7f4b4b9433974d400f8edf8ee17db0be329349a7
Tomás Arrativel <arrativeltomas@gmail.com>
2024-06-05 15:14:19 -0300

    Creating the SearchSlice slice

--- src/features/search/searchSlice.js
+++ src/features/search/searchSlice.js
@@ -1,7 +1,34 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-export const fetchSearch = createAsyncThunk('posts/fetchSearch', async (q) => {
+export const fetchSearch = createAsyncThunk('search/fetchSearch', async (q) => {
   const response = await fetch(`https://www.reddit.com/search.json?q=${q}`);
   const data = await response.json();
   return data;
+});
+
+export const SearchSlice = createSlice({
+  name: 'search',
+  initialState: {
+    posts: [],
+    loading: false,
+    error: false,
+    ups: [],
+    downs: [],
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(SearchSlice.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(SearchSlice.rejected, (state) => {
+        state.loading = false;
+        state.error = true;
+      })
+      .addCase(SearchSlice.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = false;
+        state.posts.push(action.payload);
+      });
+  },
 });
