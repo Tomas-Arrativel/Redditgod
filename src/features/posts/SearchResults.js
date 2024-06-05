e7e111175edc3b57d857f1722699af1bc1d8fbdf
Tomás Arrativel <arrativeltomas@gmail.com>
2024-06-05 15:37:00 -0300

    Connecting the slice with the frontend

--- src/features/posts/SearchResults.js
+++ src/features/posts/SearchResults.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchSearchPosts, selectResults } from './postsSlice';
+
+import './SearchResults.css';
+import Post from '../../components/Post/Post';
 
 const SearchResults = () => {
+  const dispatch = useDispatch();
+  const searchResult = useSelector(selectResults);
+
   const url = new URL(window.location);
   const q = url.searchParams.get('q');
   console.log(q);
+
+  useEffect(() => {
+    dispatch(fetchSearchPosts(q));
+  }, [dispatch]);
+
+  // console.log(searchResult.posts[0].data.children);
+  console.log(searchResult);
 
   return <div>SearchResults</div>;
 };
